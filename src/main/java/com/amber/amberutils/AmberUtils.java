038f098352c182ebc52b6c7e0dfc44a346387b27
Amber <asmit.singh2602@gmail.com>
2024-04-23 03:26:36 +0530

    Added main files

--- src/main/java/com/amber/amberutils/AmberUtils.java
+++ src/main/java/com/amber/amberutils/AmberUtils.java
@@ -1,7 +1,10 @@
 package com.amber.amberutils;
 
-import net.minecraft.*;
+import net.minecraft.command.CommandBase;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.Dependency;
 
+@Plugin(id = "amberutils", name = "AmberUtils", version = "0.0.1", description = "Utility stuff for pixelmon", dependencies = {@Dependency(id = "pixelmon")})
 class AmberUtils {
     void main(String []args) {
         System.out.println("Hi.");
