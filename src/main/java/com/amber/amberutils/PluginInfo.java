1d1095e3b84bead5a60e06a6ed14177cae05b0b2
Amber <asmit.singh2602@gmail.com>
2024-04-23 12:13:06 +0530

    Create PluginInfo.java

---
+++ src/main/java/com/amber/amberutils/PluginInfo.java
@@ -0,0 +1,12 @@
+package com.amber.amberutils;
+
+public class PluginInfo {
+
+	private PluginInfo() {
+	}
+
+	public static final String ID = "amberutils";
+	public static final String NAME = "AmberUtils";
+	public static final String VERSION = "0.0.1-alpha";
+	public static final String DESCRIPTION = "Utilities for Pixelmon and Minecraft 1.12.2";
+}
