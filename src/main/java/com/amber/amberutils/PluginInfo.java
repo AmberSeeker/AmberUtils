65bd95aa54626b7eed32b43bc941801bb7229f3b
Amber <asmit.singh2602@gmail.com>
2024-04-24 12:44:59 +0530

    Update PluginInfo.java

--- src/main/java/com/amber/amberutils/PluginInfo.java
+++ src/main/java/com/amber/amberutils/PluginInfo.java
@@ -2,16 +2,11 @@ package com.amber.amberutils;
 
 public class PluginInfo {
 
-	private PluginInfo() {
-	}
-
 	public static final String ID = "amberutils";
 	public static final String NAME = "AmberUtils";
 	public static final String VERSION = "0.0.1-alpha";
+	public static final String MC_VERSION = "1.12.2";
 	public static final String DESCR = "Utilities for Pixelmon and Minecraft 1.12.2";
-	public static final String DB_URL = "jdbc:mysql://sql6.freesqldatabase.com:3306/sql6701200";
-	public static final String DB_USER = "sql6701200";
-	public static final String DB_PASSWORD = "Zanblu9dLG";
-
+	
 }
 //i-BX4ZRMqanDZPblwi2avugQew4_xEfsGF4DUIYi-yc
