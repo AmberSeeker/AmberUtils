9052cc5bf2a95f9e372420fabffe70ffaa88236b
Amber <asmit.singh2602@gmail.com>
2024-04-24 07:50:03 +0530

    Update Commands.java
    Fixes for help command

--- src/main/java/com/amber/amberutils/commands/Commands.java
+++ src/main/java/com/amber/amberutils/commands/Commands.java
@@ -1,5 +1,9 @@
 package com.amber.amberutils.commands;
 
+import com.amber.amberutils.AmberUtils;
+import com.amber.amberutils.listeners.EventListeners;
+import com.amber.amberutils.sql_db.DatabaseManager;
+import com.amber.amberutils.helpers.ToggleHelper;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.command.CommandSource;
@@ -9,8 +13,6 @@ import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
-import com.amber.amberutils.listeners.EventListeners;
-import com.amber.amberutils.sql_db.DatabaseManager;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -20,13 +22,13 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
 
 public class Commands implements CommandExecutor {
 
-    private static final Map<UUID, Boolean> noSpaceToggles = new HashMap<>(DatabaseManager.noSpaceToggles);
-    private static boolean newValue;
+    private static final Logger logger = AmberUtils.logger;
+
+    public static final Map<UUID, Boolean> noSpaceToggles = AmberUtils.noSpaceToggles;
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) {
@@ -43,6 +45,7 @@ public class Commands implements CommandExecutor {
             .executor(new Commands())
             .child(buildNoSpaceSpec(), "nospace")
             .child(buildHelpSpec(), "help", "?")
+            .child(buildReloadSpec(), "reload")
             .build();
     }
 
@@ -55,31 +58,57 @@ public class Commands implements CommandExecutor {
                     src.sendMessage(Text.of(TextColors.RED, "Only players can use this command."));
                     return CommandResult.success();
                 }
-
                 Player player = (Player) src;
                 UUID playerId = player.getUniqueId();
-                newValue = !getNoSpaceToggle(playerId);
+                boolean newValue = !getNoSpaceToggle(playerId);
                 setNoSpaceToggle(playerId, newValue);
                 src.sendMessage(Text.of(TextColors.GOLD, "Battle regardless of Space has been set to: " + newValue));
                 return CommandResult.success();
             })
             .build();
     }
+
+    //The helper for no_space
+    private static ToggleHelper noSpaceToggle = ToggleHelper.noSpaceToggle;
     private static CommandSpec buildHelpSpec() {
+        noSpaceToggle.setValue(null);
         return CommandSpec.builder()
             .description(Text.of("Displays help for AmberUtils plugin"))
             .permission("amberutils.command.help")
             .executor((src, args) -> {
                 src.sendMessage(Text.of(TextColors.GREEN, "Available subcommands:"));
-                for (UUID playerId : noSpaceToggles.keySet()) {
-                    boolean noSpaceToggle = noSpaceToggles.get(playerId);
-                    src.sendMessage(Text.of(TextColors.YELLOW, "/amberutils nospace - Fight Wild Pokemon regardless of Space Enabled: " + noSpaceToggle));
+                if (src instanceof Player) {
+                    Player player = (Player) src;
+                    UUID playerId = player.getUniqueId();
+                    if (noSpaceToggles.get(playerId) != null)
+                    noSpaceToggle.setValue(noSpaceToggles.get(playerId));
                 }
+                src.sendMessage(Text.of(TextColors.YELLOW, "/amberutils nospace - Fight Wild Pokemon regardless of Space Enabled: " + noSpaceToggle.getValue()));
+                noSpaceToggle.setValue(null);
                 return CommandResult.success();
             })
             .build();
         }
-    private static boolean getNoSpaceToggle(UUID playerId) {
+
+    private static CommandSpec buildReloadSpec() {
+        return CommandSpec.builder()
+            .description(Text.of("Reloads configuration or data"))
+            .permission("amberutils.command.reload")
+            .executor((src, args) -> {
+                if (src instanceof Player) {
+                    src.sendMessage(Text.of(TextColors.GREEN, "Reloading AmberUtils..."));
+                    logger.info("§aReloading AmberUtils...");
+                } else {
+                    logger.info("§aReloading AmberUtils...");
+                }
+                DatabaseManager.savePlayerData(src);
+                DatabaseManager.loadPlayerData(src);
+                return CommandResult.success();
+            })
+            .build();
+        }
+
+    public static boolean getNoSpaceToggle(UUID playerId) {
         return noSpaceToggles.getOrDefault(playerId, false);
     }
     
