a525f3ed4c7ec6d35c1115955393f8ebad7c5d69
Amber <asmit.singh2602@gmail.com>
2024-04-24 07:36:33 +0530

    Create PlayerNameHelper.java
    Gets PlayerName from UUID

---
+++ src/main/java/com/amber/amberutils/helpers/PlayerNameHelper.java
@@ -0,0 +1,27 @@
+package com.amber.amberutils.helpers;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.service.user.UserStorageService;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PlayerNameHelper {
+
+    public static String getPlayerName(UUID playerId) {
+        Optional<Player> playerOptional = Sponge.getServer().getPlayer(playerId);
+        if (playerOptional.isPresent()) {
+            return playerOptional.get().getName();
+        } else {
+            // Player is not online, try to fetch their name from storage service
+            Optional<UserStorageService> userStorageServiceOptional = Sponge.getServiceManager().provide(UserStorageService.class);
+            if (userStorageServiceOptional.isPresent()) {
+                Optional<String> playerNameOptional = userStorageServiceOptional.get().get(playerId).map(user -> user.getName());
+                return playerNameOptional.orElse("Unknown"); // Return "Unknown" if player name is not found
+            } else {
+                // UserStorageService is not available
+                return "Unknown";
+            }
+        }
+    }
+}    
