25c2ede94cd27c6e7a4aaaa645a20f7f1eeeb61a
Amber <asmit.singh2602@gmail.com>
2024-04-27 07:49:11 +0530

    Bump to 0.2.1

--- src/main/java/com/amber/amberutils/PluginInfo.java
+++ src/main/java/com/amber/amberutils/PluginInfo.java
@@ -4,7 +4,7 @@ public class PluginInfo {
 
 	public static final String ID = "amberutils";
 	public static final String NAME = "AmberUtils";
-	public static final String VERSION = "0.2.0-release";
+	public static final String VERSION = "0.2.1-release";
 	public static final String MC_VERSION = "1.12.2";
 	public static final String DESCR = "Utilities for Pixelmon and Minecraft 1.12.2";
 	
